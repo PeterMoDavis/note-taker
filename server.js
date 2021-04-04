f8720836b2d355fe26dfa2345e8df0f3712c28d5
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-04-04 12:22:45 -0400

    served the landing page

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("hey there bad dog");
+  res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
