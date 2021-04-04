a2fe0c5002b4ca79eddf6695628bbd7d782e7e72
Peter MoDavis <pmodavis.webdev@gmail.com>
2021-04-04 16:33:34 -0400

    got the css to format

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
+// THEN I am presented with a page with existing notes listed in the left-hand column, plus empty fields to enter a new note title and the note’s text in the right-hand column
+// WHEN I enter a new note title and the note’s text
+// THEN a Save icon appears in the navigation at the top of the page
+// WHEN I click on the Save icon
+// THEN the new note I have entered is saved and appears in the left-hand column with the other existing notes
+// WHEN I click on an existing note in the list in the left-hand column
+// THEN that note appears in the right-hand column
+// WHEN I click on the Write icon in the navigation at the top of the page
+// THEN I am presented with empty fields to enter a new note title and the note’s text in the right-hand column
+
 const express = require("express");
+const fs = require("fs");
 const path = require("path");
 
 // Sets up the Express App
@@ -9,13 +20,29 @@ const PORT = process.env.PORT || 4000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(express.static("public"));
+//gets the landing page
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
+//gets the notes page after pushing the landing page button
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  fs.readFile("./db/db.json", "utf8", (err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.send(data);
+  });
+});
+
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+  console.log(newNote);
 });
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
